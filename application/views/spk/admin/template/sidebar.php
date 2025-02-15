ab6ca1ebaa0e1dc3e5f850c0ff2b7683f9135273
TinaRahmawati <tinarahmawati029@gmail.com>
2025-02-15 22:54:10 +0700

    menambahkan file

--- application/views/spk/admin/template/sidebar.php
+++ application/views/spk/admin/template/sidebar.php
@@ -30,56 +30,15 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?php echo base_url('ProsesPerhitungan/admin') ?>" class="nav-link <?php if ($this->uri->segment(1) == 'dashboard') echo 'active' ?>">
-                        <i class="nav-icon fas fa-calculator"></i>
+                    <a href="<?php echo base_url('result/admin') ?>" class="nav-link <?php if ($this->uri->segment(1) == 'result') echo 'active' ?>">
+                        <i class="nav-icon fas fa-file-alt"></i>
                         <p>
-                            Proses Perhitungan
+                            Result
                         </p>
                     </a>
                 </li>
                 <hr>
                 <li class="nav-header">ADMINISTRATION</li>
-                <li class="nav-item" id="questioner-menu">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>
-                            Questioner
-                            <i class="right fas fa-angle-left"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="<?php echo base_url('question/question') ?>" class="nav-link <?php if ($this->uri->segment(1) == 'question') echo 'active' ?>">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Question</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href=" <?php echo base_url('aspect/aspect') ?>" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Aspect</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="./index3.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Factor</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="./index2.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Criteria</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="./index3.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Sub Criteria</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
                 <li class="nav-item">
                     <a href="<?php echo base_url('user/user') ?>" class="nav-link <?php if ($this->uri->segment(1) == 'user') echo 'active' ?>">
                         <i class="nav-icon fas fa-users"></i>
@@ -88,16 +47,49 @@
                         </p>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a href="<?php echo base_url('userRole/user_roles') ?>" class="nav-link <?php if ($this->uri->segment(1) == 'userRole') echo 'active' ?>">
+                <li class="nav-item" id="questioner-menu">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-clipboard-list"></i>
+                        <p>
+                            Questioner
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="<?php echo base_url('question/question') ?>" class="nav-link <?php if ($this->uri->segment(1) == 'question') echo 'active' ?>">
+                                <i class="far fa-question-circle nav-icon"></i>
+                                <p>Question</p>
+                            </a>
+                        </li>
+                    </ul>
+                    <li class="nav-item">
+                    <a href="<?php echo base_url('aspect/aspect') ?>" class="nav-link <?php if ($this->uri->segment(1) == 'aspect') echo 'active' ?>">
                         <i class="nav-icon far fa-id-card"></i>
                         <p>
-                            User Roles
+                            Aspect
+                        </p>
+                    </a>
+                </li>
+                    <li class="nav-item">
+                    <a href="<?php echo base_url('criteria/criteria') ?>" class="nav-link <?php if ($this->uri->segment(1) == 'criteria') echo 'active' ?>">
+                        <i class="nav-icon far fa-id-card"></i>
+                        <p>
+                            Criteria
+                        </p>
+                    </a>
+                </li>
+                </li>           
+                <li class="nav-item">
+                    <a href="<?php echo base_url('calculation/calculation') ?>" class="nav-link <?php if ($this->uri->segment(1) == 'calculation') echo 'active' ?>">
+                        <i class="nav-icon fas fa-calculator"></i>
+                        <p>
+                            Calculation Process
                         </p>
                     </a>
                 </li>
             </ul>
-        </nav>
+                    </nav>
     </div>
 </aside>
 <script type="text/javascript">
